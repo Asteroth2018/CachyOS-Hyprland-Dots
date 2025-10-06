f4c8eb265dceb2d248d1898eec77dbbdab091a9c
Asteroth <99032904+Asteroth2018@users.noreply.github.com>
2025-10-06 14:20:50 +0200

    Add files via upload

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+fastfetch -c /home/asteroth/.config/fastfetch/config-pwsh.jsonc --logo-color-1 yellow --logo-color-2 red --logo-color-3 magenta --logo-color-4 blue
+
+Invoke-Expression (&starship init powershell)
+
+$ENV:STARSHIP_CONFIG = "/home/asteroth/.config/starship-pwsh.toml"
